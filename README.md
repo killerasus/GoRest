79dd00f21b9b011c9dbb14072ef5d004f01f5bed
Bruno Baère Pedrazzi Lomba de Araujo <killerasus@gmail.com>
2022-06-15 02:11:21 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Go REST API
+
+This program simulates a driver-sharing app like many famous apps through a REST API using [Go](golang.org) and [Gin](github.com/gin-gonic/gin).
+
+An adaptation of the examples from the book [APIs REST from Alexandre Saudate](https://www.casadocodigo.com.br/products/livro-apis-rest).
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Driver struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	Birthdate time.Time `json:"birthdate"`
+}
+
+var drivers []Driver //@TODO: Use database instead
+
+func getDrivers(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, drivers)
+}
+
+func getDriverById(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Invalid id"})
+	}
+	for _, d := range drivers {
+		if d.ID == id {
+			c.IndentedJSON(http.StatusOK, d)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Driver not found"})
+}
+
+func createDriver(c *gin.Context) {
+	var driver Driver
+	if err := c.BindJSON(&driver); err != nil {
+		return
+	}
+	drivers = append(drivers, driver)
+	c.IndentedJSON(http.StatusCreated, gin.H{"message": "Driver created"})
+}
+
+func main() {
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "home.tmpl", gin.H{"message": "Hello CAR World!"})
+	})
+	r.GET("/driver", getDrivers)
+	r.GET("/driver/:id", getDriverById)
+	r.POST("/driver", createDriver)
+
+	r.Run("localhost:8080")
+}
